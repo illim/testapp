65a49b95578552d6216123a6c0b811a0e1d72f28
illim <illminouche@gmail.com>
2013-11-25 18:16:33 +0100

    map

--- public/js/app.js
+++ public/js/app.js
@@ -2,24 +2,29 @@
 
 'use strict';
 
+require.config({
+    paths: {
+        async: 'lib/require/async'
+    }
+});
+
 // Declare here that angular is the US version - other locales can be easily substituted.
 
-define('angular', ['webjars!angular-locale_en-us.js'], function() {
-    return angular;
-});
+define('angular', ['webjars!angular-locale_en-us.js'], function() {  return angular; });
+
+define('map', ['async!http://maps.google.com/maps/api/js?sensor=false'], function() { return google.maps ;});
 
 require(['angular', './controllers', './directives', './filters', './services'],
-  function(angular, controllers) {
+        function(angular, controllers) {
 
-// Declare app level module which depends on filters, and services
+            // Declare app level module which depends on filters, and services
 
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
-  
-angular.bootstrap(document, ['myApp']);
+            angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+                config(['$routeProvider', function($routeProvider) {
+                    $routeProvider.when('/community', {templateUrl: 'partials/partial2.html', controller: controllers.ComCtrl});
+                    $routeProvider.otherwise({redirectTo: '/community'});
+                }]);
 
-});
+            angular.bootstrap(document, ['myApp']);
+
+        });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,18 +1,37 @@
 /*global define */
-
 'use strict';
-
-define(function() {
-
-/* Controllers */
+define(["map"], function() {
 
 var controllers = {};
+var user = {
+    location : {}
+};
 
-controllers.MyCtrl1 = function() {}
-controllers.MyCtrl1.$inject = [];
-
-controllers.MyCtrl2 = function() {}
-controllers.MyCtrl2.$inject = [];
+controllers.ComCtrl = function($scope) {
+    $scope.address = "";
+    var mapOptions = {
+//        center: new google.maps.LatLng(48.833, 2.33),
+        zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+                                      mapOptions);
+    var geocoder = new google.maps.Geocoder();
+    $scope.find = function(){
+        geocoder.geocode( { 'address': $scope.address }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                user.location = results[0].geometry.location;
+                map.setCenter(user.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: user.location
+                });
+            } else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    };
+};
+controllers.ComCtrl.$inject = ['$scope'];
 
 return controllers;
 
